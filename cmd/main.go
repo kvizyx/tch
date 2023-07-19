210b46a87a0e1107e458a0b405d1050a3c44253b
kvizyx <kirill.sudakov.01@mail.ru>
2023-07-19 20:25:19 +0300

    feat: add banned names handling

--- cmd/main.go
+++ cmd/main.go
@@ -12,13 +12,27 @@ var (
 	ErrNoPathsSpecified = color.RedString("Error: No paths specified")
 	ErrCannotCreateDirs = color.RedString("Error: Cannot create directories")
 	ErrCannotCreateFile = color.RedString("Error: Cannot create file")
+	ErrBannedName       = color.RedString("Error: Cannot use this name")
 )
+
+// Names that are better not to name a folder or file in Windows
+var bannedNames = []string{
+	"con", "prn", "aux", "nul", "com0",
+	"com1", "com2", "com3", "com4", "com5",
+	"com6", "com7", "com8", "com9", "lpt0",
+	"lpt1", "lpt2", "lpt3", "lpt4", "lpt5",
+	"lpt6", "lpt7", "lpt8", "lpt9",
+}
 
 func main() {
 	args := os.Args[1:]
 
 	if len(args) == 0 {
 		errfatal.Print(ErrNoPathsSpecified)
+	}
+
+	if hasEqualStrings(args, bannedNames) {
+		errfatal.Print(ErrBannedName)
 	}
 
 	for _, path := range args {
@@ -33,4 +47,20 @@ func main() {
 		}
 		file.Close()
 	}
+}
+
+func hasEqualStrings(first, second []string) bool {
+	table := make(map[string]bool, len(first))
+
+	for _, s := range first {
+		table[s] = true
+	}
+
+	for _, s := range second {
+		if table[s] {
+			return true
+		}
+	}
+
+	return false
 }
