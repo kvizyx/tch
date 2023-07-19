5af696d7eb7bb141814c672b240bffebc640451f
kvizyx <kirill.sudakov.01@mail.ru>
2023-07-19 18:45:33 +0300

    style: remove unnecessary comments

--- libs/errfatal/errfatal.go
+++ libs/errfatal/errfatal.go
@@ -7,13 +7,11 @@ import (
 
 // This package is remake of log.Fatal() without "log style" output
 
-// Print prints message and then call os.Exit(1)
 func Print(message string) {
 	fmt.Println(message)
 	os.Exit(1)
 }
 
-// Printf prints formatted message and then call os.Exit(1)
 func Printf(message string, args ...any) {
 	fmt.Println(fmt.Sprintf(message, args))
 	os.Exit(1)
